c6cfa8b259ea009531e25e7243ff104164cad676
jennifer-fazler <jennifer.fazler@thermofisher.com>
2022-03-18 14:24:37 -0400

    S2.12 disable form submit button

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -19,8 +19,8 @@ export class LoginComponent implements OnInit {
 
   }
 
-  login(loginForm: NgForm) {
-    console.log(loginForm.value, loginForm.valid);
+  login(loginForm: NgForm, submit) {
+    console.log(loginForm.value, loginForm.valid, submit);
   }
 
 }
