1f6d20c96d2af2b00f753662ac1e52e7085d23a5
jennifer-fazler <jennifer.fazler@thermofisher.com>
2022-03-19 12:35:29 -0400

    S4.32 selection dropdown

--- src/app/create-course/create-course-step-1/create-course-step-1.component.ts
+++ src/app/create-course/create-course-step-1/create-course-step-1.component.ts
@@ -5,6 +5,11 @@ import {Observable} from 'rxjs';
 import {filter} from 'rxjs/operators';
 import { courseTitleValidator } from '../../validators/course-title.validator';
 
+interface CourseCategory {
+  code:string,
+  description:string;
+}
+
 @Component({
   selector: 'create-course-step-1',
   templateUrl: './create-course-step-1.component.html',
@@ -23,15 +28,20 @@ export class CreateCourseStep1Component implements OnInit {
       updateOn: 'blur'
     }],
     releasedAt: [new Date(), Validators.required],
+    category: ['BEGINNER', Validators.required],
     downloadsAllowed: [false, Validators.requiredTrue],
     longDescription: ['', [Validators.required, Validators.minLength(3)]]
   });
+
+  courseCategories$ : Observable<CourseCategory[]>;
 
   constructor(private fb: FormBuilder, private courses:CoursesService) {
 
   }
 
   ngOnInit() {
+
+    this.courseCategories$ = this.courses.findCourseCategories();
 
   }
 
