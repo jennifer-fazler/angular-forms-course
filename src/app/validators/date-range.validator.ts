de1c52958c843ac571a068a8cda65b553dea9d69
jennifer-fazler <jennifer.fazler@thermofisher.com>
2022-03-19 14:49:39 -0400

    S4.36 multi-field validation

---
+++ src/app/validators/date-range.validator.ts
@@ -0,0 +1,17 @@
+import { getLocaleDateFormat } from "@angular/common";
+import { FormGroup, ValidatorFn, Validators } from "@angular/forms";
+
+export function createPromoRangeValidator(): ValidatorFn {
+  return (form: FormGroup): Validators | null => {
+    const start:Date = form.get("promoStartAt").value;
+    const end:Date = form.get("promoEndAt").value;
+
+    if (start && end) {
+      const isRangeValid = (end?.getTime() - start?.getTime() > 0);
+
+      return isRangeValid ? null : {promoPeriod:true};
+    }
+
+    return null;
+  }
+}
